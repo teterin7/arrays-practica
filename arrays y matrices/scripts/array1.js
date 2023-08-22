efc49158898a186d846af612d878c03b2cc4b30c
teterin7 <jaimemorenoedp@gmail.com>
2023-08-22 09:37:23 -0500

    arrays

---
+++ arrays y matrices/scripts/array1.js
@@ -0,0 +1,22 @@
+// crea un array
+
+
+let ArrayNombres= new Array(5)
+
+// ArrayNombres[0]="juan";
+// ArrayNombres[1]="pedro";
+// ArrayNombres[2]="ana";
+// ArrayNombres[3]="pepe";
+// ArrayNombres[4]="maria";
+
+// console.log(ArrayNombres)
+
+
+for (let i = 0; i < ArrayNombres.length;i++ )  {
+ArrayNombres[i]=prompt("ingrese el nombre")
+
+}
+for (let i= 0; i<ArrayNombres.length;i++){
+    ArrayNombres[i]
+}
+console.log(ArrayNombres)
